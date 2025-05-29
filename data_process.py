3c78308e6818209e32d7536768ae81a00276477f
Madvith-d <madroux930@gmail.com>
2025-05-29 09:22:36 +0530

    added data processs

---
+++ data_process.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+
+df1= pd.read_csv("data/daily_sales_data_0.csv")
+df2=pd.read_csv("data/daily_sales_data_1.csv")
+df3=pd.read_csv("data/daily_sales_data_2.csv")
+
+combined_df= pd.concat([df1,df2,df3], ignore_index=True)
+
+combined_df.to_csv("data/combined_data.csv", index=False)
+
+
+df =pd.read_csv("data/combined_data.csv")
+df= df[df["product"]=="pink morsel"]
+
+df.to_csv("data/pink_morsel.csv",index=False)
+
+df=pd.read_csv("data/pink_morsel.csv")
+df["sales"]=df["quantity"]*df["price"]
+print(df)
